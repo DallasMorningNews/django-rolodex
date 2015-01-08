91caee163735ae4ee807409d3f5148ceda042348
Jon <jon.r.mcclure@gmail.com>
2015-01-08 15:43:15 -0600

    database routing on fixtures fixed the problem

--- rolodex/migrations/0002_fixtures.py
+++ rolodex/migrations/0002_fixtures.py
@@ -4,10 +4,8 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 from django.conf import settings
-from __future__ import print_function
 
 def initial_data(apps, schema_editor):
-	print("Rolodex Fixtures")
 	if settings.DATABASES.has_key('rolodex'):
 		if not schema_editor.connection.alias == 'rolodex':
         	return
