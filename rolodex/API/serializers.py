c2b96e241987a379fd8eec129d20b54117a7f874
Jon <jon.r.mcclure@gmail.com>
2015-01-08 11:03:05 -0600

    updated serializers for djangorestframework v. 3.0 ... explicit querysets

--- rolodex/API/serializers.py
+++ rolodex/API/serializers.py
@@ -33,7 +33,7 @@ class P2Org_TypeSerializer(serializers.HyperlinkedModelSerializer):
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
 	people = serializers.HyperlinkedRelatedField(many=True, read_only=True,view_name='person-relations')
 	orgs = serializers.HyperlinkedRelatedField(many=True, read_only=True,view_name='org-relations')
-	person_role = serializers.HyperlinkedRelatedField(many=True, read_only=True,view_name='roles')
+	person_role = serializers.HyperlinkedRelatedField(many=True, read_only=False,view_name='roles',queryset=PersonRole.objects.all())
 	class Meta:
 		model = Person
 		fields = ('id','firstName', 'lastName',  'position','department','gender','p_relations','org_relations','role','person_contact')
@@ -46,40 +46,40 @@ class OrgSerializer(serializers.HyperlinkedModelSerializer):
 		fields = ('id','orgName','org_relations','p_relations','org_contact')
 
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
-	person_contact = serializers.HyperlinkedRelatedField( read_only=False,view_name='person')
-	org_contact = serializers.HyperlinkedRelatedField( read_only=False,view_name='org')
+	person_contact = serializers.HyperlinkedRelatedField( read_only=False,view_name='person',queryset=Person.objects.all())
+	org_contact = serializers.HyperlinkedRelatedField( read_only=False,view_name='org',queryset=Org.objects.all())
 	class Meta:
 		model = Contact
 		fields = ('id','person','org','type','contact','role','notes')
 
 class P2PSerializer(serializers.HyperlinkedModelSerializer):
-	p_from_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_person')
-	p_to_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_person')
-	p2p_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type')
+	p_from_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_person',queryset=Person.objects.all())
+	p_to_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_person',queryset=Person.objects.all())
+	p2p_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type',queryset=P2P_Type.objects.all())
 	class Meta:
 		model = P2P
 		fields = ('id','from_ent','to_ent','relation','from_date','to_date','description')
 
 class Org2OrgSerializer(serializers.HyperlinkedModelSerializer):
-	org_from_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_org')
-	org_to_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_org')
-	org2org_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type')
+	org_from_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_org',queryset=Org.objects.all())
+	org_to_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_org',queryset=Org.objects.all())
+	org2org_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type',queryset=Org2Org_Type.objects.all())
 	class Meta:
 		model = Org2Org
 		fields = ('id','from_ent','to_ent','relation','from_date','to_date','description')
 
 class P2OrgSerializer(serializers.HyperlinkedModelSerializer):
-	org_from_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_person')
-	p_to_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_org')
-	p2org_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type')
+	org_from_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_person',queryset=Person.objects.all())
+	p_to_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_org',queryset=Org.objects.all())
+	p2org_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type',queryset=P2Org_Type.objects.all())
 	class Meta:
 		model = P2Org
 		fields = ('id','from_ent','to_ent','relation','from_date','to_date','description')
 
 class Org2PSerializer(serializers.HyperlinkedModelSerializer):
-	p_from_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_org')
-	org_to_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_person')
-	org2p_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type')
+	p_from_org = serializers.HyperlinkedRelatedField( read_only=False,view_name='from_org',queryset=Org.objects.all())
+	org_to_p = serializers.HyperlinkedRelatedField( read_only=False,view_name='to_person',queryset=Person.objects.all())
+	org2p_relation = serializers.HyperlinkedRelatedField( read_only=False,view_name='relationship_type',queryset=P2Org_Type.objects.all())
 	class Meta:
 		model = Org2P
 		fields = ('id','from_ent','to_ent','relation','from_date','to_date','description')
